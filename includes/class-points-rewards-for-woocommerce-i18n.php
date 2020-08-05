5cf96e830ec9234aa2a412a3b5271e0eb71b03ef
SamiullahPk <63010159+SamiullahPk@users.noreply.github.com>
2020-08-06 00:25:22 +0600

    Add files via upload

---
+++ includes/class-points-rewards-for-woocommerce-i18n.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://makewebbetter.com/
+ * @since      1.0.0
+ *
+ * @package    points-and-rewards-for-wooCommerce
+ * @subpackage points-and-rewards-for-wooCommerce/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    points-and-rewards-for-wooCommerce
+ * @subpackage points-and-rewards-for-wooCommerce/includes
+ * @author     makewebbetter <webmaster@makewebbetter.com>
+ */
+class Points_Rewards_For_Woocommerce_I18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'points-and-rewards-for-woocommerce',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
